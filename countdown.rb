8245cbb4fff2d962223bc3a5d2a5e22907629ecf
Peter Ort <peter@peterort.com>
2020-04-03 19:31:44 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -4,7 +4,17 @@ def countdown(number)
   while number >0
     puts "#{number} SECOND(S)!"
     number -= 1
-    #sleep(1)
+  end
+  return "HAPPY NEW YEAR!"
+end
+
+number = 12
+
+def countdown_with_sleep(number)
+  while number >0
+    puts "#{number} SECOND(S)!"
+    number -= 1
+    sleep(1)
   end
   return "HAPPY NEW YEAR!"
 end
